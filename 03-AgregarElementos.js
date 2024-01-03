eb57e9ca507dcd896891209b53fdb5b295d6fee9
Manuel F <maneflo02@gmail.com>
2024-01-03 14:26:20 -0600

    Cambio4

---
+++ 03-AgregarElementos.js
@@ -0,0 +1,15 @@
+const notas = [8, 8, 6];
+
+
+let media = (notas[0] + notas[1]+notas[2])/notas.length;
+console.log(notas);
+
+//agregando elementos al final 
+notas.push(10);
+//notas.unshift(10);
+
+media = (notas[0] + notas[1]+notas[2])/notas.length;
+
+console.log(notas);
+
+
