86e47a1cf1f4784e7b8bde710b4bae876d10d292
Manuel F <maneflo02@gmail.com>
2024-01-03 20:28:36 -0600

    Clase6

---
+++ 07-Concatenar.js
@@ -0,0 +1,11 @@
+const listPHP = ["Lucia", "Maria", "Daniel", "Martin","Maria","sofia"];
+const listNodeJS = ["Alvaro", "Valeria", "Alba", "Julia"];
+const listNode = ["Alva", "Vale", "Alba", "Lalo"];
+
+const listUnificado = listPHP.concat(listNodeJS, listNode);
+console.log(listUnificado);
+console.log(listNodeJS);
+console.log(listPHP);
+console.log(listNode);
+
+
