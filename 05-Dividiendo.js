f1eb47e56a157472b3eab568395b355d486375b6
Manuel F <maneflo02@gmail.com>
2024-01-03 19:51:50 -0600

    Clase5

---
+++ 05-Dividiendo.js
@@ -0,0 +1,12 @@
+const listAlumnos =["Lucia", "Maria", "Daniel", "Martin","Maria",
+                     "sofia", "Pablo", "Alejandro", "Paula", "Daniela",
+                     "Lucas", "Alvaro", "Valeria", "Alba", "Julia", 
+                     "Noa","Adrian", "Mateo", "David", "Hugo"];
+
+const grupoA = listAlumnos.slice(0,10);
+const grupoB = listAlumnos.slice(10,20);
+
+console.log(grupoA);
+console.log(grupoB);
+
+console.log(listAlumnos);
---
+++ 06-Splice.js
@@ -0,0 +1,6 @@
+const listAlumnos =["Lucia", "Maria", "Daniel", "Martin","Maria",
+                     "sofia", "Pablo"];
+console.log(listAlumnos);
+listAlumnos.splice(2,2,"Lalo","Fer");
+
+console.log(listAlumnos);
