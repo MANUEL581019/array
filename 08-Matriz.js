dc440890fff8913a406f7481514d5025084ca437
Manuel F <maneflo02@gmail.com>
2024-01-03 21:12:38 -0600

    Clase7

---
+++ 08-Matriz.js
@@ -0,0 +1,7 @@
+const listAlumnos = ["Alvaro", "Valeria", "Alba", "Julia"];
+const listNotas = [8, 9, 10, 8];
+
+
+const listUnificada =[listAlumnos, listNotas];
+console.log(listUnificada[1][2]);
+
